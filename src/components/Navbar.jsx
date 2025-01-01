0a40f6965f0954e7449dbb6d8454aa1f0ccc06f4
Mubarek Adem <mubarekademy@gmail.com>
2025-01-01 12:45:00 +0300

    fixed the dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { FaSun, FaMoon, FaHome, FaInfoCircle } from "react-icons/fa";
-//new
+//new darkmode
 const Navbar = ({ isDarkMode, toggleDarkMode }) => {
   return (
     <nav className="bg-gray-800 dark:bg-gray-900 text-white p-4 fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/" className="text-2xl font-bold text-green-400">
-          Zikr App
+          My-Zikr App
         </Link>
         <div className="flex items-center space-x-4">
           <Link
